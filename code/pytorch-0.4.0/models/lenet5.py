e21b995cb614b30f539e87be443beaf88531b163
Heegreis <run3113214@gmail.com>
2019-05-04 03:08:40 +0800

    put difference version pytorch code into same branch

--- code/pytorch-0.4.0/models/lenet5.py
+++ code/pytorch-0.4.0/models/lenet5.py
@@ -19,7 +19,7 @@ class LeNet(nn.Module):
         # Max pooling over a (2, 2) window
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
         # If the size is a square you can only specify a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
