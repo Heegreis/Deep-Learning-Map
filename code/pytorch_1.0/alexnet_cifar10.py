1add3f49fcca860541f13df67facc11b283dd8fb
Heegreis <run3113214@gmail.com>
2019-06-09 21:28:37 +0800

    將train跟test的資料載入分開寫

--- code/pytorch_1.0/alexnet_cifar10.py
+++ code/pytorch_1.0/alexnet_cifar10.py
@@ -2,26 +2,35 @@ import torch
 from torchvision import datasets, models, transforms
 
 
+def test(model, device, test_loader):
+    pass
+
+
 def main():
     # 載入dataset : CIFAR10 (torchvision包裝好的)
     transform = None
-    cifar10_datasets = {
-        x: datasets.CIFAR10(
-            root='../data/CIFA10',
-            train=True,
-            transform=transform,
-            download=True)
-        for x in ['train', 'val']
-    }
-    dataloaders = {
-        x: torch.utils.data.DataLoader(
-            cifar10_datasets[x], batch_size=4, shuffle=True, num_workers=2)
-        for x in ['train, val']
-    }
-    dataset_sizes = {x: len(cifar10_datasets) for x in ['train', 'val']}
+    cifar10_train_set = datasets.CIFAR10(root='../data/CIFA10',
+                                         train=True,
+                                         transform=transform,
+                                         download=True)
+    cifar10_test_set = datasets.CIFAR10(root='../data/CIFA10',
+                                        train=False,
+                                        transform=transform,
+                                        download=True)
+    train_loader = torch.utils.data.DataLoader(cifar10_train_set,
+                                               batch_size=4,
+                                               shuffle=True,
+                                               num_workers=2)
+    test_loader = torch.utils.data.DataLoader(cifar10_test_set,
+                                              batch_size=4,
+                                              shuffle=True,
+                                              num_workers=2)
 
     # 設定device參數
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    #載入模型
+    model = models.alexnet(pretrained=True, progress=True)
 
 
 if __name__ == "__main__":
